505dffabbacae0ceba0e71513cff1c5a2b54b269
Eseoghene Erigha <ese.erigha@gmail.com>
2024-01-14 15:11:31 -0500

    chore: remove react-helmet

--- src/app/providers.tsx
+++
@@ -1,7 +0,0 @@
-'use client';
-
-import { HelmetProvider } from 'react-helmet-async';
-
-export default function Providers({ children }: { children: React.ReactNode }) {
-  return <HelmetProvider>{children}</HelmetProvider>;
-}
