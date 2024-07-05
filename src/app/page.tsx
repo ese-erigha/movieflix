2f86e900297af27d6b9a92e624649e24af3c90a5
Ese Erigha <ese.erigha@gmail.com>
2024-07-05 03:01:49 -0400

    Revert "chore: generate uid in main page"
    This reverts commit 8840a88142d4bf3c3c0b088f8c3def5e8ff05424.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,6 @@
 import { redirect } from 'next/navigation';
+import { generateUid } from './common/helper';
 
 export default function Home() {
-  const min = Math.ceil(1);
-  const max = Math.floor(611);
-  const uid =  Math.floor(Math.random() * (max - min + 1)) + min;
-  redirect(`/user/${uid}/1`);
+  redirect(`/user/${generateUid()}/1`);
 }
