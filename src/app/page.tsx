8840a88142d4bf3c3c0b088f8c3def5e8ff05424
Ese Erigha <ese.erigha@gmail.com>
2024-07-04 21:28:09 -0400

    chore: generate uid in main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 import { redirect } from 'next/navigation';
-import { generateUid } from './common/helper';
 
 export default function Home() {
-  redirect(`/user/${generateUid()}/1`);
+  const min = Math.ceil(1);
+  const max = Math.floor(611);
+  const uid =  Math.floor(Math.random() * (max - min + 1)) + min;
+  redirect(`/user/${uid}/1`);
 }
