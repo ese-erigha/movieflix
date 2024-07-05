ffa4b08b7ab7d4007840d4f58794bf7a18f7ab2f
Ese Erigha <ese.erigha@gmail.com>
2024-07-05 03:22:22 -0400

    chore: create redirect component

---
+++ src/app/components/redirect.tsx
@@ -0,0 +1,17 @@
+'use client';
+
+import { useRouter } from 'next/navigation';
+import { useEffect } from "react";
+import { generateUid } from '../common/helper';
+import LoadingSpinner from './loading';
+
+export default function Redirect() {
+    const router = useRouter();
+
+    useEffect(() => {
+
+        router.push(`/user/${generateUid()}/1`);
+    }, [router])
+
+    return <LoadingSpinner/>
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
-import { redirect } from 'next/navigation';
-import { generateUid } from './common/helper';
+// import { redirect } from 'next/navigation';
+// import { generateUid } from './common/helper';
+import Redirect from './components/redirect';
 
 export default function Home() {
-  redirect(`/user/${generateUid()}/1`);
+  // redirect(`/user/${generateUid()}/1`);
+  return <Redirect/>
 }
